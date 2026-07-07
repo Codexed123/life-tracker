ab1faec6a3bc148f21dfc127023decc340a3fd5e
C o d e x <codexvosture@gmail.com>
2026-07-07 23:12:38 +0800

    Delete life_tracker_web directory

--- life_tracker_web/life_tracker_content/models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-# Create your models here.
-
-class ActivitySerializer(models.Model):
-    title = models.CharField(max_length=100)
-    description = models.CharField(max_length=100)
-    completed = models.BooleanField(default=False)
-    timestamp = models.DateTimeField(auto_now=True)
-
--- life_tracker_web/life_tracker_content/serializers.py
+++
@@ -1,7 +0,0 @@
-from rest_framework import serializers
-from life_tracker_content.models import ActivitySerializer
-
-class ActivityViewSet(serializers.ModelSerializer):
-    class Meta:
-        model = ActivitySerializer
-        fields = '__all__'
